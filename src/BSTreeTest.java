19bd3248a9d4f2b77c27996e28f3bf8e31585f65
kenogihara <kenogihara808@gmail.com>
2024-02-21 12:02:15 -0800

    bruh

---
+++ src/BSTreeTest.java
@@ -0,0 +1,48 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class BSTreeTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getRoot() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSize() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void insert() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findKey() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void insertData() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findDataList() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findHeight() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void iterator() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void intersection() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void levelMax() {
+    }
+}
